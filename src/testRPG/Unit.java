b399f2272cb2547b3ebc233514c5525d6260233e
bang9859 <kokozz9859@gmail.com>
2024-11-14 17:19:19 +0900

    Add variable and constructor

--- src/testRPG/Unit.java
+++ src/testRPG/Unit.java
@@ -1,5 +1,20 @@
 package testRPG;
 
-public class Unit {
-
+abstract public class Unit {
+	private String name;
+	private int hp;
+	private int power;
+	private int shield;
+	
+	protected Unit(String name, int hp, int power, int shield){
+		this.name = name;
+		this.hp = hp;
+		this.power = power;
+		this.shield = shield;
+	}
+	
+	protected void Attack(Unit target) {
+		
+	}
+	
 }
