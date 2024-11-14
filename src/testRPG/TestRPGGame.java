f08cf1739ec3dad2493b7445bf9602c8846d9750
bang9859 <kokozz9859@gmail.com>
2024-11-14 15:25:54 +0900

    Create TestRPGGame and TestRPGSystem

---
+++ src/testRPG/TestRPGGame.java
@@ -0,0 +1,10 @@
+package testRPG;
+
+public class TestRPGGame {
+
+	public static void main(String[] args) {
+		TestRPGSystem game = TestRPGSystem.getInstance();
+		game.run();
+	}
+
+}
---
+++ src/testRPG/TestRPGSystem.java
@@ -0,0 +1,16 @@
+package testRPG;
+
+public class TestRPGSystem {
+	private TestRPGSystem() {
+	}
+
+	private static TestRPGSystem instance = new TestRPGSystem();
+
+	protected static TestRPGSystem getInstance() {
+		return instance;
+	}
+	
+	protected void run() {
+		
+	}
+}
