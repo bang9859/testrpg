7e66cbf57e2c1efbb0d0fecc9199361ffa757b47
bang9859 <kokozz9859@gmail.com>
2024-11-15 14:07:47 +0900

    Feat input and printOut method

--- src/testRPG/TestRPGSystem.java
+++ src/testRPG/TestRPGSystem.java
@@ -10,6 +10,12 @@ public class TestRPGSystem {
 	private final int STRING = 1;
 	private final int NUMBER = 2;
 
+	private final int DUNGEON = 1;
+	private final int SETTING = 2;
+	private final int EXIT = 3;
+
+	private boolean isRun = true;
+
 	private TestRPGSystem() {
 	}
 
@@ -25,48 +31,98 @@ public class TestRPGSystem {
 
 	protected void run() {
 		gameStartMessage();
+		while (isRun) {
+			Lobby();
+		}
+		printOut("게임을 종료합니다.");
+	}
 
-//		while(isRun) {
-//			
-//		}
+	private void Lobby() {
+		LobbyMenu();
+		int sel = (int) input("메뉴선택", NUMBER);
+		if (sel == DUNGEON) {
+			dengeon();
+		} else if (sel == SETTING) {
+			setting();
+		} else if (sel == EXIT) {
+			isRun = false;
+		}
+
+	}
+
+	private void dengeon() {
+
+	}
+
+	private void setting() {
+		printOut("=====[SETTING]=====");
+		printOut("[1.길드관리] [2.상점] [3.인벤토리]");
+		printOut("[4.저장] [5.로드] [6.종료] [0. 뒤로가기]\n");
+
+	}
+
+	private void printOut(String msg) {
+		buffer.setLength(0);
+		buffer.append(msg+"\n");
+		try {
+			writer.append(buffer);
+			writer.flush();
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
+
+	private void LobbyMenu() {
+		printOut("=====[LOBBY]=====");
+		printOut("[1.던전] [2.설정] [3.종료]\n");
 	}
 
 	private void gameStartMessage() {
+		printOut("<<<더조은 RPG GAME>>>");
+		printOut("시작하시려면 엔터를 눌러주세요.\n");
+		while (true) {
+			try {
+				String input = reader.readLine();
+				if (input.equals(""))
+					break;
+			} catch (IOException e) {
+				e.printStackTrace();
+			}
+
+		}
+		loading("게임 로딩중...\n", 1000);
+
+	}
+
+	private void loading(String msg, int time) {
 		buffer.setLength(0);
-		buffer.append("<<<더조은 RPG GAME>>>\n");
-		buffer.append("시작하시려면 엔터를 눌러주세요.\n");
+		buffer.append(msg + "\n");
 		try {
 			writer.append(buffer);
 			writer.flush();
 		} catch (IOException e) {
 			e.printStackTrace();
-		} finally {
-			try {
-			} catch (Exception e2) {
-				e2.printStackTrace();
-			}
 		}
-		while (true) {
-			String input = (String) input("입력", STRING);
-			if (input.equals(""))
-				break;
+		try {
+			Thread.sleep(time);
+		} catch (Exception e) {
+			e.printStackTrace();
 		}
-		System.out.println("다음스테이지 이동~");
-
 	}
 
 	private Object input(String msg, int n) {
 		buffer.setLength(0);
-		buffer.append(msg+" : ");
+		buffer.append(msg + " : ");
 		String input = "";
 		try {
 			writer.append(buffer);
+			writer.flush();
 			input = reader.readLine();
 
 		} catch (Exception e) {
 			e.printStackTrace();
-		} 
-		
+		}
+
 		if (n == STRING) {
 			return input;
 		} else {
